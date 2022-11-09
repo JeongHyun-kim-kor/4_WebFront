889f7e51ecbec1c27132d0e87e2be3eefc94a57c
jeonghyun <jhyeon310@naver.com>
2022-11-09 11:08:17 +0900

    1109 2교시

--- 3_Javascript/js/11_window내장객체.js
+++ 3_Javascript/js/11_window내장객체.js
@@ -37,3 +37,44 @@ btn1.addEventListener("click", function(){
     }, 9000);
 
 });
+
+
+// 현재 시간을 반환하는 함수
+function currentTime(){
+    const now = new Date(); // 현재 시간 정보를 저장하고 있는 객체 생성
+
+    let hour = now.getHours();
+    let min = now.getMinutes();
+    let sec = now.getSeconds();
+
+    // 3초 -> 03초로 나오게끔 
+    // 얻어온 시간 값이 10미만일 경우 앞에 0추가
+    if(hour < 10) hour = "0" + hour;
+    if(min < 10) min = "0" + min;
+    if(sec < 10) sec = "0" + sec;
+
+    return hour + " : " + min + " : " + sec;
+}
+
+const clock = document.getElementById("clock");
+// 페이지가 로딩 되자마자 #clock에 현재 시간을  출력하도록 하는 함수 출력
+
+
+clock.innerText = currentTime(); 
+
+
+  // #clock에 1초마다 현재 시간을 출력하도록하는 코드
+const interval = window.setInterval(function(){
+    clock.innerText = currentTime();
+}, 1000);
+
+// window.clearInterval(setInerval()이 저장된 변수)
+document.getElementById("stop").addEventListener("click", function(){
+
+    window.clearInterval(interval);
+
+});
+
+
+
+
